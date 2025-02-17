e4f06739888e68da5d79a4bc37b72e92cc93f77c
jdaly3411 <jdaly3411@gmail.com>
2025-02-17 23:05:25 +0000

    Commit

--- BackEnd/memory_api/memories/views.py
+++ BackEnd/memory_api/memories/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Memory
 from .serializers import MemorySerializer
+import openai
 
 class MemoryListCreateView(generics.ListCreateAPIView):
     queryset = Memory.objects.all()
     serializer_class = MemorySerializer
+
+
+def generateEmbed(text):
+    response = openai.Embedding.create(input=text, model = "text-embedding-ada-002")
+    return response["data"][0]["embedding"]
--- BackEnd/memory_api/memory_api/settings.py
+++ BackEnd/memory_api/memory_api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import pinecone
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000"
 ]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+pinecone.init(api_key="pcsk_2biFYr_KPaBGKEfBJwgMXgVvqReKLgtJMVTs1MEZYMJeT1uDKa6J4o8gbPMyYTtjrTgVKf")
